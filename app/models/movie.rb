76e7942ef253887cd4179877ed9db943fc3c0e0f
Patrick Young <patrickyoung281@gmail.com>
2022-11-03 18:35:19 -0400

    commit tree 6972e58c1d75dabf44138f256b89fa7776b2b362

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
 
+def self.create_with_title(title)
+    Movie.create(title: title)
+end
+
+def self.first_movie
+    all = Movie.all
+    all.first 
+end
+
+def self.last_movie
+    all=Movie.all
+    all=Movie.last
+end
+
+def self.movie_count
+    all = Movie.all
+    all.count 
+end
+
+def self.find_movie_with_id(id)
+    Movie.find(id==id)
+end
+
+def self.find_movie_with_attributes(attributes)
+    Movie.find(attributes==attributes)
+end
+
+def self.find_movies_after_2002
+    Movie.where("release_date > 2002")
+end
+
+def update_with_attributes(title)
+    self.update(title)
+end
+
+def self.update_all_titles(title)
+    Movie.update(title: title)
+end
+
+def self.delete_by_id(id)
+    destroy(id==id)
+end
+
+def self.delete_all_movies
+    Movie.destroy_all
+end
+
 end
